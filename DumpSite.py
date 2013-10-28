9a0c0232a6f922a36ed76f655969553539b68c8c
Matt Lovett <mattlovett@mattlovett.com>
2013-10-27 19:39:50 -0700

    Incremental improvements
    Broke out the main file transfer code into a seperate function for
    improved readability.
    
    Improved logging and exception capturing.

--- DumpSite.py
+++ DumpSite.py
@@ -8,6 +8,7 @@ import logging
 import datetime
 import glob
 import shutil
+import atexit
 
 debug_level = "DEBUG" #DEBUG or INFO
 enable_pushover = True  #Enable pushover support to send a notification when completed
@@ -20,8 +21,9 @@ folder_to_dump = "Downloads"  #Path to folder to dump relative to mount point
 dump_location = "/storage/Downloads"  #aboslute location to dump files to
 clean_dumptruck = False #If true source folder will be emptied, If false a copy will remain in the source folder
 
-#pushover.pushover(message="Hello, world",token = app_token,user = user_token,)
+atexit.register(endprog)
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='/var/log/dumpsite.log',level=logging.DEBUG)
+logging.handlers.TimedRotatingFileHandler(filename='/var/log/dumpsite.log', when='midnight',backupCount=7, encoding=None, delay=False, utc=False)
 logging.info('DumpSite service started')
 
 
@@ -36,77 +38,51 @@ class DeviceAddedListener:
 		device_obj = self.bus.get_object ("org.freedesktop.Hal", udi)
 		device = dbus.Interface(device_obj, "org.freedesktop.Hal.Device")
 
-		if device.QueryCapability("volume"):
+		if device.QueryCapability("volume"): #if the dbus event is a device with a mountable volume lets use it
 			return self.do_something(device)
 	def do_something(self, volume):
+		#getting the detected device's info	
 		device_file = volume.GetProperty("block.device")
 		label = volume.GetProperty("volume.label")
 		fstype = volume.GetProperty("volume.fstype")
 		mounted = volume.GetProperty("volume.is_mounted")
 		mount_point = volume.GetProperty("volume.mount_point")
 		try:
-			size = volume.GetProperty("volume.size")
+			size = volume.GetProperty("volume.size")      
 		except:
 			size = 0
 			
-		print "New storage device detectec:"
-		print "  device_file: %s" % device_file
-		print "  label: %s" % label
-		print "  fstype: %s" % fstype
+		#log the detected devices info if debugging enabled
+		logging.debug("New storage device detected:")     
+		logging.debug("  device_file: %s" % device_file)
+		logging.debug("  label: %s" % label)
+		logging.debug("  fstype: %s" % fstype)
 		if mounted:
-			print "  mount_point: %s" % mount_point
+			logging.debug("  mount_point: %s" % mount_point)
 		else:
-			print "  not mounted"
-		print "  size: %s (%.2fGB)" % (size, float(size) / 1024**3)
+			logging.debug("  not mounted")
+		logging.debug("  size: %s (%.2fGB)" % (size, float(size) / 1024**3))
 
-		return_code = subprocess.call(["mount", "-t" , fstype, device_file, mount_location])
-		print(str(return_code))
+		#Lets try and mount the SOB, if it works we should get a return of 0
+		return_code = subprocess.call(["mount", "-t" , fstype, device_file, mount_location]) 
+		
 		if return_code == 0:
-			logging.info('drive mounted successesfully!')
-			if os.path.exists(mount_location + "/" + folder_to_dump):
-				logging.info("Found a folder to dump from")
-				
-				number_to_dump = len(glob.glob(mount_location + "/" + folder_to_dump + "/*"))  #find the number of files in the folder to be dumped
-				logging.info("there are " + str(number_to_dump) + " items to dump")  #log the number of files
-				source = os.listdir(mount_location+"/"+folder_to_dump+"/")
-				for files in source:
-					logging.debug("copying " + mount_location+"/"+folder_to_dump+"/"+files +" to " + dump_location)
-					try:
-						if clean_dumptruck  == True:
-							print("move")#move files instead of copy
-						elif clean_dumptruck == False:
-							shutil.copy(mount_location + "/" + folder_to_dump + "/" + files ,dump_location)
-					except IOError, e:
-						logging.warning("Unable to copy file. %s" %e)
-				#	except shutil.Error, exc:
-				#		errors = exc.args[0]
-				#		for error in errors:
-				#			src, dst, msg = error
-				if unmount_on_finish:
-					subprocess.call(["umount",device_file])
-					logging.debug(device_file + " unmounted")
-					pushover.pushover(message= str(number_to_dump) + "successfully dumped to " + dump_location,token = app_token,user = user_token,)
-
-			else:
-				logging.info("Found no folder to dump from")
-				if unmount_on_fail:
-					subprocess.call(["umount",device_file])
-					logging.debug(device_file + " unmounted")
-				
+			transfer()
+		#mount return/failure codes		
 		if return_code == 1:
-			logging.debug('incorrect invocation or permissions')
+			logging.warning('incorrect invocation or permissions')
 		elif return_code == 2:
-			logging.debug('system error, out of memory, cannot fork, no more loop devices')
+			logging.warning('system error, out of memory, cannot fork, no more loop devices')
 		elif return_code == 4:
-			logging.debug('internal mount bug or missing nfs support in mount')
+			logging.warning('internal mount bug or missing nfs support in mount')
 		elif return_code == 8:
-			logging.debug('user interrupt')
+			logging.warning('user interrupt')
 		elif return_code == 16:
-			logging.debug('problems writing or locking /etc/mtab')
+			logging.warning('problems writing or locking /etc/mtab')
 		elif return_code == 32:
-			logging.info('mount failure')
+			logging.warning('mount failure')
 		elif return_code == 64:
-			logging.debug('some mount succeded')
+			logging.warning('some mount succeded')
 		else:
 			logging.warning('mount failed for an unknown reason, mount code: ' + str(return_code))
 			
@@ -116,3 +92,46 @@ if __name__ == '__main__':
 	loop = gobject.MainLoop()
 	DeviceAddedListener()
 	loop.run()
+
+#routine that does the file transfers	
+def transfer()
+logging.info('drive mounted successesfully!')
+if os.path.exists(mount_location + "/" + folder_to_dump): #check if the mounted drive has a folder to get stuff from
+	logging.info("Found a folder to dump from") 
+	number_to_dump = len(glob.glob(mount_location + "/" + folder_to_dump + "/*"))  #find the number of files in the folder to be dumped
+	if number_to_dump > 0:  #if there are files lets dump em, otherwise what are we doing here?
+		logging.info("there are " + str(number_to_dump) + " items to dump")  #log the number of files
+		source = os.listdir(mount_location+"/"+folder_to_dump+"/")	#dump the filenames to be moved to an array
+		for files in source:
+			logging.debug("copying " + mount_location+"/"+folder_to_dump+"/"+files +" to " + dump_location) #call off the transfer with from and to
+			#if the user wants a clean dumptruck move the files, otherwise just copy the files
+			try:
+				if clean_dumptruck  == True:
+					shutil.move(mount_location + "/" + folder_to_dump + "/" + files ,dump_location) #copy files
+				elif clean_dumptruck == False:
+					shutil.copy(mount_location + "/" + folder_to_dump + "/" + files ,dump_location) #copy files
+			except shutil.Error, err_msg:
+				logging.warning("Unable to copy file:" + files) #rutrow something went wrong...this is as good as it gets now, eventually better debugging
+				logging.warning(err_msg)
+		logging.info("done transfering files, see you next time")
+		if unmount_on_finish:  #if user elected to unmount on finish then boot that drive out of the system
+			subprocess.call(["umount",device_file])
+			logging.info(device_file + " unmounted")
+			#Let pushover know what we have done here, lets hope they are not disapoinited in us
+			pushover.pushover(message= str(number_to_dump) + " successfully dumped to " + dump_location,token = app_token,user = user_token,)
+	else:
+		#if the users wants an unmount on a soft fail then the dude abides
+		logging.info("Found nothing to dump")
+		if unmount_on_fail:
+			subprocess.call(["umount",device_file])
+			logging.info(device_file + " unmounted")
+		
+else:
+	#if the users wants an unmount on a soft fail then the dude abides
+	logging.info("Found no folder to dump from")
+	if unmount_on_fail:
+		subprocess.call(["umount",device_file])
+		logging.info(device_file + " unmounted")
+		
+def endprog()
+	logging.info('DumpSite service stopping')
